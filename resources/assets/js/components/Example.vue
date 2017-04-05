f291dfc199f4b345ee0940bad6ce23110812d78e
rohimma <maikel.s@hotmail.com>
2017-04-06 01:58:13 +0200

    alfa!

--- resources/assets/js/components/Example.vue
+++ resources/assets/js/components/Example.vue
@@ -1,23 +1,180 @@
 <template>
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Example Component</div>
+            <div class="col-md-12">
+                <textarea v-model="text" class="form-control" rows="3"></textarea>
 
-                    <div class="panel-body">
-                        I'm an example component!
-                    </div>
+                <div class="info-text">Click on the field to copy the text to your clipboard:</div>
+
+                <textarea id="clipboard-1" v-model="upSideDown" :data-clipboard-text="upSideDown" class="form-control" rows="3" disabled></textarea>
+                <textarea id="clipboard-2" v-model="putBack" :data-clipboard-text="putBack" class="form-control" rows="3" v-if="addTrowGuy" disabled></textarea>
+
+                <div id="buttons">
+                    <button class="btn btn-default" @click="reverse = !reverse">{{ reverseMsg }}</button>
+                    <button class="btn btn-default" @click="addTrowGuy = !addTrowGuy">{{ addTrowGuyMsg }}</button>
                 </div>
             </div>
         </div>
     </div>
 </template>
 
+<style>
+    .info-text {
+        margin-top: 15px;
+        margin-bottom: 15px;
+    }
+
+    #buttons {
+        margin-top: 15px;
+    }
+
+    #clipboard-2 {
+        margin-top: 15px;
+    }
+</style>
+
 <script>
     export default {
         mounted() {
-            console.log('Component mounted.')
+            console.log('Component mounted.');
+
+            var clipboard1 = new Clipboard('#clipboard-1');
+            var clipboard2 = new Clipboard('#clipboard-2');
+        },
+        data() {
+            return {
+                text: '',
+                reverse: true,
+                addTrowGuy: false
+            }
+        },
+        methods: {
+            reverseTxt: function(txt) {
+                return txt.split('').reverse().join('')
+            }
+        },
+        computed: {
+            putBack() {
+                return this.text + " ノ( ゜-゜ノ)";
+            },
+            upSideDown() {
+                let retVal = this.text;
+
+                retVal = retVal
+                // uppercase
+                        .split('A').join('Ɐ')
+                        .split('B').join('ᗺ')
+                        .split('C').join('Ɔ')
+                        .split('D').join('ᗡ')
+                        .split('E').join('Ǝ')
+                        .split('F').join('ᖵ')
+                        .split('G').join('⅁')
+                        .split('H').join('H')
+                        .split('I').join('I')
+                        .split('J').join('ᒋ')
+                        .split('K').join('⋊')
+                        .split('L').join('Ꞁ')
+                        .split('M').join('W')
+                        .split('N').join('N')
+                        .split('O').join('O')
+                        .split('P').join('Ԁ')
+                        .split('Q').join('Ꝺ')
+                        .split('R').join('ᴚ')
+                        .split('S').join('S')
+                        .split('T').join('⊥')
+                        .split('U').join('∩')
+                        .split('V').join('Ʌ')
+                        .split('W').join('M')
+                        .split('X').join('X')
+                        .split('Y').join('⅄')
+                        .split('Z').join('Z')
+
+                        // lowercase
+                        .split('a').join('ɐ')
+                        .split('b').join('q')
+                        .split('c').join('ɔ')
+                        .split('d').join('p')
+                        .split('e').join('ǝ')
+                        .split('f').join('ɟ')
+                        .split('g').join('ƃ')
+                        .split('h').join('ɥ')
+                        .split('i').join('ᴉ')
+                        .split('j').join('ɾ')
+                        .split('k').join('ʞ')
+                        .split('l').join('ꞁ')
+                        .split('m').join('ɯ')
+                        .split('n').join('u')
+                        .split('o').join('o')
+                        .split('p').join('d')
+                        .split('q').join('b')
+                        .split('r').join('ɹ')
+                        .split('s').join('s')
+                        .split('t').join('ʇ')
+                        .split('u').join('n')
+                        .split('v').join('ʌ')
+                        .split('w').join('ʍ')
+                        .split('x').join('x')
+                        .split('y').join('ʎ')
+                        .split('z').join('z')
+
+                        // numbers
+                        .split('1').join('І')
+                        .split('2').join('ᘔ')
+                        .split('3').join('Ɛ')
+                        .split('4').join('ᔭ')
+                        .split('5').join('5')
+                        .split('6').join('9')
+                        .split('7').join('Ɫ')
+                        .split('8').join('8')
+                        .split('9').join('6')
+                        .split('0').join('0')
+
+                        // special chars
+                        .split('.').join('˙')
+                        .split(',').join('\'')
+                        .split('\'').join(',')
+                        .split('"').join(',,')
+                        .split('`').join(',')
+                        .split('<').join('>')
+                        .split('>').join('<')
+                        .split('∴').join('∵')
+                        .split('&').join('⅋')
+                        .split('@').join('@')
+                        .split('~').join('~')
+                        .split('_').join('‾')
+                        .split('?').join('¿')
+                        .split('!').join('¡')
+                        .split('[').join(']')
+                        .split(']').join('[')
+                        .split('(').join(')')
+                        .split(')').join('(')
+                        .split('{').join('}')
+                        .split('}').join('{');
+
+                if (this.reverse) {
+                    retVal = this.reverseTxt(retVal);
+                }
+
+                if (this.addTrowGuy) {
+                    retVal = "(╯°□°)╯︵ " + retVal;
+                }
+
+                return retVal;
+            },
+            reverseMsg() {
+                if (!this.reverse) {
+                    return 'Reverse message';
+                }
+
+                return 'Don\'t reverse message';
+            },
+            addTrowGuyMsg() {
+                if (!this.addTrowGuy) {
+                    return 'Add: (╯°□°)╯︵ ';
+                }
+
+                return 'Remove: (╯°□°)╯︵ ';
+            }
         }
     }
 </script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,91 +5,43 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>{{ config('app.name') }}</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link href="/css/app.css" rel="stylesheet" type="text/css">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <script>
+            // rename myToken as you like
+            window.Laravel =  <?php echo json_encode([
+                    'csrfToken' => csrf_token(),
+            ]); ?>
+        </script>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
-
+        <div id="app" class="flex-center position-ref full-height">
             <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+                <div class="m-b-md">
+                    <span class="title">{{ config('app.name') }}</span>
+                    <span style="font-size: 15px">For all your upsidedown needs!</span>
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <example></example>
+                <div id="footer">
+                    inspiration from
+                    <a href="https://tweetfleet.slack.com/messages/@craqgerbil" target="_blank">@craqgerbil</a>
+                    on
+                    <a href="https://tweetfleet.slack.com/" target="_blank">#tweetfleet</a>
+                    <br />
+                    <div class="github-logo">
+                        <a href="https://github.com/rohimma/upsidedowntext.online" target="_blank">
+                            <svg aria-hidden="true" height="32" version="1.1" viewBox="0 0 16 16" width="32">
+                                <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
+                            </svg>
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
+
+        <script src="/js/app.js"></script>
     </body>
 </html>
